1fa47b7cdd6bacaa514daaa91011e5292edca8e6
alan_khang <alan_khang@alan.luu>
2025-08-16 20:00:26 +0700

    update

--- test_det3d.py
+++ test_det3d.py
@@ -1,22 +1,23 @@
 import torch
 import cv2
+import numpy as np
 
 from mmengine.structures import InstanceData
 from mmdet3d.structures import Det3DDataSample
 from mmdeploy.apis.utils import build_task_processor
 from mmdeploy.utils import get_input_shape, load_config
 
-deploy_cfg = './configs/mmdet3d/monocular-detection/fcos3d_adam_tensorrt_static-416x640.py'
+deploy_file_cfg = './configs/mmdet3d/monocular-detection/fcos3d_adam_tensorrt_static-416x640.py'
 model_cfg = '/home/alan_khang/dev/mmdetection3d/work_dirs/fcos3d_adam/fcos3d_r101-caffe-dcn_fpn_head-gn_8xb2-1x_adam-mono3d.py'
 device = 'cuda:0'
 backend_model = ['work_dir/mmdet3d/fcos3d/end2end.engine']
 image_path = '/home/alan_khang/Downloads/guilder_dataset/images/frame_000000.jpg'
-image = cv2.imread(image_path)
+image = cv2.imread(image_path)[..., ::-1]  # Convert BGR to RGB
+image = np.ascontiguousarray(image)
 
 assert image is not None, f"Image not found at {image_path}"
 
-# read deploy_cfg and model_cfg
-deploy_cfg, model_cfg = load_config(deploy_cfg, model_cfg)
+deploy_cfg, model_cfg = load_config(deploy_file_cfg, model_cfg)
 
 # build task and backend model
 task_processor = build_task_processor(model_cfg, deploy_cfg, device)
@@ -39,10 +40,18 @@ if isinstance(result, dict):
     result = Det3DDataSample(metainfo=metadata)
     result.pred_instances_3d = pred_instances_3d
 
+print(f"Result 3d boxes: {result.pred_instances_3d.bboxes_3d.tensor[:3]}")
+
+drawed_image = task_processor.draw_bboxes(image, result)
+
+cv2.imshow('drawed_image', drawed_image[..., ::-1])  # Convert RGB to BGR for OpenCV
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 # visualize results
-task_processor.visualize(
-    image=image,
-    result=result,
-    window_name='visualize',
-    output_file='output_detection.png',
-    show=True)
+    #task_processor.visualize(
+    #    image=image,
+    #    result=result,
+    #    window_name='visualize',
+    #    output_file='output_detection.png',
+    #    show=True)
